317c2677264b2ce09539ca517fddb30c122c887b
Patryk Dabrowski <patrykdabrowski58@gmail.com>
2020-12-04 13:59:26 +0100

    Upload image

--- semantic-ui/index.js
+++ semantic-ui/index.js
@@ -1,7 +1,28 @@
+const uploadBtn = document.querySelector(".upload-btn");
+const uploadInput = document.querySelector(".upload-input");
+const image = document.querySelector("img.image");
+
+const uploadImage = () => {
+  uploadInput.click();
+};
+
+function handleFile() {
+  const file = this.files[0];
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    image.src = reader.result;
+  };
+  reader.readAsDataURL(file);
+}
+
 window.onload = () => {
   "use strict";
 
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("./sw.js");
   }
+
+  uploadBtn.addEventListener("click", uploadImage);
+  uploadInput.addEventListener("change", handleFile);
 };
