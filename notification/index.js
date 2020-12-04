66ccd4acdf8614d848d9256116a36e108314606c
Patryk Dabrowski <patrykdabrowski58@gmail.com>
2020-12-04 13:06:58 +0100

    Add notification

---
+++ notification/index.js
@@ -0,0 +1,35 @@
+let swRegistration = null;
+
+window.onload = () => {
+  "use strict";
+
+  if ("serviceWorker" in navigator && "PushManager" in window) {
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then((swReg) => {
+        swRegistration = swReg;
+      })
+      .catch((error) => console.error("Service Worker Error", error));
+  }
+};
+
+function notifyMe() {
+  if (!("Notification" in window)) {
+    alert("Ta przeglądarka nie obsługuje powiadomień");
+  } else if (Notification.permission === "granted") {
+    notification();
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission((permission) => {
+      if (permission === "granted") {
+        notification();
+      }
+    });
+  }
+}
+
+function notification() {
+  const options = {
+    body: "Test test",
+  };
+  swRegistration.showNotification("Test", options);
+}
